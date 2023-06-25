315e4cf44f08a3054337376884eb6c2505294001
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-25 16:36:34 +0530

    conponent added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListBookComponent } from './components/list-book/list-book.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListBookComponent },
+  { path: 'list-book', component: ListBookComponent },
+  { path: 'add-book', component: AddBookComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { ListBookComponent } from './components/list-book/list-book.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    ListBookComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
