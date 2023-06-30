1a2781681d0d39f2ef17b70f2fe01cd685afdec2
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-30 21:02:36 +0530

    Add book using formModule

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Book, addBookAction } from 'src/app/store/bookstore.reducer';
+import { MyAppState } from 'src/app/store/root.reducer';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.scss']
+  styleUrls: ['./add-book.component.scss'],
 })
-export class AddBookComponent {
+export class AddBookComponent implements OnInit {
+  newBook: Book = {
+    imageUrl: '',
+    title: '',
+    purchaseLink: '',
+    PublishDate: '',
+  };
 
+  constructor(private store: Store<MyAppState>) {}
+
+  ngOnInit(): void {}
+
+  addBook() {
+    let newBook = {
+      imageUrl: this.newBook.imageUrl,
+      title: this.newBook.title,
+      purchaseLink: this.newBook.purchaseLink,
+      PublishDate: this.newBook.PublishDate,
+    };
+    this.store.dispatch(addBookAction({ newBook: this.newBook }));
+  }
 }
